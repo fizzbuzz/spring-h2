e87571c637b7e5f6fd985b2ecf5447010f452e1e
fizzbuzz <fizzbuzz@users.noreply.github.com>
2014-03-21 14:01:40 +0100

    First commit

---
+++ src/main/java/a/b/c/Employee.java
@@ -0,0 +1,41 @@
+package a.b.c;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+
+@Entity
+public class Employee {
+
+	@Id
+	@GeneratedValue(strategy=GenerationType.AUTO)
+	Integer id;
+	String name;
+	public Integer getId() {
+		return id;
+	}
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	@ManyToOne(cascade = CascadeType.PERSIST)
+//	@OneToOne(cascade = CascadeType.PERSIST)
+	@JoinColumn(name="DEPT_ID")
+	Department department;
+	public Department getDepartment() {
+		return department;
+	}
+	public void setDepartment(Department department) {
+		this.department = department;
+	}
+}
